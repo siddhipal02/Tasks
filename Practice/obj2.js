afe24a0a3eee6d9b75788a0c3f75f36159b779f8
siddhipal02 <siddhi.pal@habilelabs.io>
2025-06-18 11:10:32 +0530

    more questions

--- Practice/obj2.js
+++ Practice/obj2.js
@@ -25,3 +25,19 @@ employee.department.totalHours(employee.department.projects);
 //add new skill to skills array
  employee.skills[2] = "Java"
  console.log(employee.skills);
+
+ //Get project titles where hours > 100
+ for(let i of employee.department.projects)
+ {
+    if(i.hours > 100)
+    {
+        console.log(i.title);
+    }
+ }
+
+ //Return: Aarav works in Development department and knows JavaScript, React, Java
+ console.log(employee.name + " works in " + employee.department.name + " department and knows " + employee.skills.toString());
+
+ //Sort projects by hours (descending).
+ employee.department.projects.sort((a, b) => b.hours - a.hours);
+ console.log(employee.department.projects);
