2c1e6c697c33efda766c827118ccd67756095b67
siddhipal02 <siddhi.pal@habilelabs.io>
2025-06-23 12:59:45 +0530

    closure examples

--- Practice/closures.js
+++ Practice/closures.js
@@ -1,3 +1,4 @@
+//example 1
 // function outer() {
 //     const outerVar = 'I am from outer';
 
@@ -12,16 +13,46 @@
 // newClosure();
 
 
+//example 2
+// function counter() {
+//     let count = 0; 
+//     return function () {
+//         count++;
+//         return count;
+//     };
+// }
 
-function counter() {
-    let count = 0; 
-    return function () {
-        count++;
-        return count;
-    };
+// const increment = counter();
+// console.log(increment());
+// console.log(increment());
+// console.log(increment());
+
+
+
+//example 3
+function createBankAccount() {
+  let balance = 1000;
+
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log(balance);
+    },
+    withdraw(amount) {
+      if (amount <= balance) {
+        balance -= amount;
+        console.log(balance);
+      } else {
+        console.log("Insufficient funds");
+      }
+    }
+  };
 }
 
-const increment = counter();
-console.log(increment());
-console.log(increment());
-console.log(increment());
+const myAccount = createBankAccount();
+myAccount.deposit(500);   //  Balance becomes 1500
+myAccount.withdraw(300);  //  Balance becomes 1200
+
+//The variable balance is enclosed (remembered) by the inner functions deposit and withdraw. So any change made to balance inside one method is reflected in the other, because they both share the same balance variable from the outer scope (createBankAccount).
+
+
